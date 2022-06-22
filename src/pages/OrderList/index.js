7f2793d88129346c115b96b5dbe6b1d321d04f3f
lisdrl <lisandre.darioli@gmail.com>
2022-06-22 10:33:52 -0300

    feat: creation of modal with user's info

--- src/pages/OrderList/index.js
+++ src/pages/OrderList/index.js
@@ -13,10 +13,11 @@
     function OrderList() {
         const [orders, setOrders] = useState([])
         const [openModal, setOpenModal] = useState(false)
+        const [user, setUser] = useState()
         const [token] = useState(localStorage.getItem('token') || '')
         const { get } = useRequestOrder();
         const { getOne } = useRequestUser();
-        const { setActualPage, usersList, setUsersList } = useGlobal();
+        const { setActualPage, usersList } = useGlobal();
 
         const [itensPerPage] = useState(10);
         const [currentPage, setCurrentPage] = useState(0);
@@ -25,6 +26,14 @@
         const starIndex = currentPage * itensPerPage;
         const endIndex = starIndex + itensPerPage;
         const currentOrders = orders.slice(starIndex, endIndex);
+
+        const getUser = (userId) => {
+            const resp = getOne('/user', userId, true).then((response) => {
+                console.log("usuario: ", response);
+                setUser(response)
+                setOpenModal(true);
+            })
+        }
 
         const goBack = () => {
             if (currentPage === 0) {
@@ -51,19 +60,6 @@
             setActualPage("Lista de Pedidos");
         }, [])
 
-        // Retornar nome do usuário, socorro Deus
-        // useEffect(() => {
-        //     orders.map((order) => {
-        //         const resp = get('user', order.userId, true).then((response) => {
-        //             setUsersList(() => {
-        //                 const newList = [...usersList];
-        //                 return [...newList, response]
-        //             });
-        //         })
-        //     })
-        // }, [orders]);
-
-
         return (
             <section className='section_orders'>
                 <div>
@@ -85,7 +81,9 @@
                                     <th className='table_id' order="row">{order.id}</th>
                                     <td 
                                         className='table_th table_td user_td'
-                                        onClick={() => setOpenModal(true)}
+                                        onClick={() => {
+                                            getUser(order.userId);
+                                        }}
                                     >
                                         {order.userId}
                                     </td>
@@ -121,17 +119,25 @@
                         <BsArrowRightCircle className={currentPage === pages - 1 ? "arrow-icon-inactive" : "arrow-icon-active"} />
                     </button>
                 </div>
-                {openModal && (
+                {openModal && user ? (
                     <div className='backdrop'>
                         <div className='user-modal'>
-                            <div className="use-modal_header">
-                                <h1>Detalhes do Cliente</h1>
+                            <div className="user-modal_header">
+                                <h1>Detalhes do cliente</h1>
                                 <AiOutlineCloseCircle className="close" onClick={() => setOpenModal(false)} alt="Ícone fechar" />
+                            </div>
+                            <div className='user-modal_info'>
+                                <span><b>Id:</b><br/> {user.id ? user.id : ""}</span>
+                                <span><b>Nome:</b><br/> {user.name ? user.name : ""}</span>
+                                <span><b>Email:</b><br/> {user.email ? user.email : ""}</span>
+                                <span><b>Telefone:</b><br/> {user.phone ? `${(user.phone).slice(0, 2)} ${(user.phone).slice(2, 3)} ${(user.phone).slice(3, 7)} ${(user.phone).slice(7)}` : ""}</span>
+                                <span><b>CPF:</b><br/> {user.cpf ? `${(user.cpf).slice(0, 3)} ${(user.cpf).slice(3, 6)} ${(user.cpf).slice(6, 9)} ${(user.cpf).slice(9)}` : ""}</span>
+                                <span><b>Data de Nascimento:</b><br/> {user.birthday ? `${user.birthday.slice(-2)}/${user.birthday.slice(-5, -3)}/${user.birthday.slice(0, 4)}` : ""}</span>
                             </div>
 
                         </div>
                     </div>
-                )}
+                ) : ""}
             </section>
         )
     }
